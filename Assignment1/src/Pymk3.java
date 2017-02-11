c135bc25b746ba2d40177e88e73fd06130574e7b
srikanth121 <srikanthtad14@gmail.com>
2017-02-10 22:01:52 -0500

    Setters and Getters + Array representation method for Reducer-2 class.

--- Assignment1/src/Pymk3.java
+++ Assignment1/src/Pymk3.java
@@ -92,4 +92,56 @@ public class Pymk3 {
         }
     }
 
+    public static class Reducer2 extends Reducer<IntWritable, IntWritable, IntWritable, Text> {
+
+        // A private class to describe a recommendation.
+        // A recommendation has a friend id and a number of friends in common.
+        private static class Recommendation {
+
+            // Attributes
+            private int friendId;
+            private int nCommonFriends;
+
+            // Constructor
+            public Recommendation(int friendId) {
+                this.friendId = friendId;
+                // A recommendation must have at least 1 common friend
+                this.nCommonFriends = 1;
+            }
+
+            // Getters
+            public int getFriendId() {
+                return friendId;
+            }
+
+            public int getNCommonFriends() {
+                return nCommonFriends;
+            }
+
+            // Other methods
+            // Increments the number of common friends
+            public void addCommonFriend() {
+                nCommonFriends++;
+            }
+
+            // String representation used in the reduce output
+            public String toString() {
+                return friendId + "(" + nCommonFriends + ")";
+            }
+
+            // Finds a representation in an array
+            public static Recommendation find(int friendId, ArrayList<Recommendation> recommendations) {
+                for (Recommendation p : recommendations) {
+                    if (p.getFriendId() == friendId) {
+                        return p;
+                    }
+                }
+                // Recommendation was not found!
+                return null;
+            }
+        }
+
+
 }
+}
+
